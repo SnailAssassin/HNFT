5497b77af72cc50573ca633b09d489877c9d3fad
SnailAssassin <mikeuniverse8@gmail.com>
2024-06-17 20:07:40 +0000

    commiting fiorst python scripts

---
+++ addition_in_python/specify_two_numbers.py
@@ -0,0 +1,12 @@
+x = input("Type a number: ")
+y = input("Type another number: ")
+
+print("Type of x: ", type(x))
+print("Type of y: ", type(y))
+
+sum = int(x) + int(y)
+
+print("\nType of sum: ", type(sum))
+
+
+print("The sum is: ", sum)
